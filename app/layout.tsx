96b2bd567d4bef5bff170db45e77c0b7477b81b8
Vishal772-pixel <fantasydark001@gmail.com>
2025-05-06 13:38:07 +0530

    improved meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next'
+
+import { Analytics } from "@vercel/analytics/react"
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Growth Tracker ',
+  description: 'Created with growth tracker team ',
+  generator: 'team',
 }
 
 export default function RootLayout({
